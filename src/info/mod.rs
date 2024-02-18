ae8b384a28195033031f8bb232a82945225d56b9
Zistack <Zistack@null.net>
2024-02-18 16:38:59 -0500

    Added macro for suppling trait forwarding information for traits in external crates

--- src/info/mod.rs
+++ src/info/mod.rs
@@ -5,6 +5,7 @@ mod transform_use;
 mod trait_def_info;
 mod trait_impl_info;
 mod type_info;
+mod supply_trait_info;
 
 pub use trait_def_info::{TraitDefInfo, forwardable_impl};
 pub use trait_impl_info::TraitImplInfo;
@@ -16,3 +17,4 @@ pub use type_info
 	MemberInfoTupleStruct,
 	forward_receiver_impl
 };
+pub use supply_trait_info::supply_forwarding_info_for_trait_impl;
---
+++ src/info/supply_trait_info.rs
@@ -0,0 +1,48 @@
+use syn::{Visibility, Path, Token, parse};
+use syn::parse::{Result, Error};
+use syn_derive::Parse;
+use quote::quote;
+
+use super::TraitDefInfo;
+use crate::uncurry::{uncurry_macro_ident, get_trait_ident, gen_uncurry_macro};
+
+#[derive (Parse)]
+struct SupplyForwardingInfoForTrait
+{
+	visibility: Visibility,
+	forwarded_trait: Path,
+	_comma: Token! [,],
+	trait_def_info: TraitDefInfo
+}
+
+fn try_supply_forwarding_info_for_trait_impl (input: proc_macro::TokenStream)
+-> Result <proc_macro2::TokenStream>
+{
+	let SupplyForwardingInfoForTrait
+	{
+		visibility,
+		forwarded_trait,
+		trait_def_info,
+		..
+	}
+		= parse (input)?;
+
+	let use_statement = quote! (#visibility use #forwarded_trait;);
+
+	let uncurry_macro = gen_uncurry_macro
+	(
+		visibility,
+		uncurry_macro_ident (&get_trait_ident (&forwarded_trait)?),
+		trait_def_info
+	);
+
+	Ok (quote! (#use_statement #uncurry_macro))
+}
+
+pub fn supply_forwarding_info_for_trait_impl (input: proc_macro::TokenStream)
+-> proc_macro::TokenStream
+{
+	try_supply_forwarding_info_for_trait_impl (input)
+		. unwrap_or_else (Error::into_compile_error)
+		. into ()
+}
